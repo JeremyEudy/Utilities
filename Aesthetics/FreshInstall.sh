2b462a4047053052c1a745d05a0e3915bf5cd995
Jeremy Eudy <jeremyeudy@gmail.com>
2019-07-12 11:56:32 -0400

    Reivised FreshInstall.sh

--- Aesthetics/FreshInstall.sh
+++ Aesthetics/FreshInstall.sh
@@ -4,10 +4,10 @@
 echo "Installing ZShell and startup commands..."
 sudo apt update
 sudo apt install zsh fortune cowsay lolcat -y
-sudo chsh -s /usr/local/bin/zsh
+chsh -s /usr/bin/zsh
 
 echo "Installing Oh-My-ZSH..."
-git clone https://github.com/robbyrussell/oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Configuring ZSH add-ons and cloning git repos..."
 mkdir ~/Git\ Clones/
@@ -30,6 +30,9 @@ cp Utilities/SSH/config ~/.ssh
 cp -r Utilities/Vim\ Stuff/templates ~/.vim/
 cp Utilities/Vim\ Stuff/.vimrc ~
 cp Utilities/tmux\ Stuff/.tmux.conf ~
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 echo "Installing glances..."
 sudo apt install glances -y
