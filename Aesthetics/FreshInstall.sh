c44b7bbe8e076f3fa85b8c7f7be8865fdaf4e1b1
Jeremy Eudy <jeremyeudy@gmail.com>
2019-08-29 16:36:45 -0400

    Finished

--- Aesthetics/FreshInstall.sh
+++ Aesthetics/FreshInstall.sh
@@ -66,7 +66,7 @@ echo "--------------------------------------------------------------------------
 echo -e "${BLUE}Configuring ZSH plugins...${NC}"
 echo "--------------------------------------------------------------------------------------"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-/home/$USER/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-/home/$USER/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo "--------------------------------------------------------------------------------------"
 echo -e "${BLUE}Copying .zshrc...${NC}"
