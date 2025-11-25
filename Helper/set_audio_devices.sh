557233970a146d67fb1777855ad07de91f020c40
Jeremy Eudy <jeremyeudy@gmail.com>
2025-11-25 11:41:12 -0500

    Updated for nixos unstable (use wpctl)

--- Helper/set_audio_devices.sh
+++ Helper/set_audio_devices.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 
+# Get IDs from wpctl status
 for i in "$@"
 do
     case $i in
         mixer)
-            pactl set-default-sink alsa_output.usb-Allen___Heath_ZEDi10-00.Recording__Line1__sink && \
-                pactl set-default-source alsa_input.usb-Allen___Heath_ZEDi10-00.Recording__Mic2__source
+            # 106. alsa_output.usb-Allen___Heath_ZEDi10-00.Recording__Line1__sink
+            # 111. alsa_input.usb-Allen___Heath_ZEDi10-00.Recording__Mic2__source
+            wpctl set-default 106 && \
+                wpctl set-default 111
             ;;
         speakers)
-            pactl set-default-sink alsa_output.pci-0000_31_00.4.analog-stereo && \
-                pactl set-default-source alsa_input.usb-Corsair_Corsair_VOID_PRO_Wireless_Gaming_Headset-00.mono-fallback
-            ;;
-        phones)
-            pactl set-default-sink alsa_output.usb-Corsair_Corsair_VOID_PRO_Wireless_Gaming_Headset-00.analog-stereo && \
-                pactl set-default-source alsa_input.usb-Corsair_Corsair_VOID_PRO_Wireless_Gaming_Headset-00.mono-fallback
+            # 57. Starship/Matisse HD Audio Controller Analog Stereo
+            wpctl set-default 57
             ;;
         guitar)
-            pactl set-default-sink alsa_output.usb-Allen___Heath_ZEDi10-00.Recording__Line1__sink && \
-                pactl set-default-source alsa_input.usb-Allen___Heath_ZEDi10-00.Recording__Mic1__source
+            # 113. alsa_input.usb-Allen___Heath_ZEDi10-00.Recording__Mic1__source
+            wpctl set-default 106 && \
+                wpctl set-default 113
             ;;
-        vr)
-            pactl set-default-sink ALVR\ Audio && \
-                pactl set-default-source ALVR\ Microphone
-            ;;
+        # vr)
+        #     pactl set-default-sink ALVR\ Audio && \
+        #         pactl set-default-source ALVR\ Microphone
+        #     ;;
         *)
             ;;
     esac
