1c35677c7c450f85e30804aa22f5eb519a0dbb4b
Jeremy Eudy <jeremyeudy@gmail.com>
2025-11-26 16:07:18 -0500

    Updated to find IDs in script since they're ephemeral

--- Helper/set_audio_devices.sh
+++ Helper/set_audio_devices.sh
@@ -1,27 +1,42 @@
 #!/usr/bin/env bash
 
 # Get IDs from wpctl status
+SPEAKERS="Starship/Matisse HD Audio Controller Analog Stereo"
+SPEAKERS_ID=$(wpctl status | grep "$SPEAKERS" | awk -F'.' '{print $1}' | awk '{print $NF}')
+
+MIXER_OUT="alsa_output.usb-Allen___Heath_ZEDi10-00.Recording__Line1__sink \[Audio/Sink\]"
+MIXER_OUT_ID=$(wpctl status | grep "$MIXER_OUT" | awk -F'.' '{print $1}' | awk '{print $NF}')
+
+MIC_IN="alsa_input.usb-Allen___Heath_ZEDi10-00.Recording__Mic2__source \[Audio/Source\]"
+MIC_IN_ID=$(wpctl status | grep "$MIC_IN" | awk -F'.' '{print $1}' | awk '{print $NF}')
+
+GUITAR_IN="alsa_input.usb-Allen___Heath_ZEDi10-00.Recording__Mic1__source \[Audio/Source\]"
+GUITAR_IN_ID=$(wpctl status | grep "$GUITAR_IN" | awk -F'.' '{print $1}' | awk '{print $NF}')
+
+# VR_OUT=""
+# VR_OUT_ID=""
+
+# VR_IN=""
+# VR_IN_ID=""
+
 for i in "$@"
 do
     case $i in
         mixer)
-            # 106. alsa_output.usb-Allen___Heath_ZEDi10-00.Recording__Line1__sink
-            # 111. alsa_input.usb-Allen___Heath_ZEDi10-00.Recording__Mic2__source
-            wpctl set-default 106 && \
-                wpctl set-default 111
+            wpctl set-default $MIXER_OUT_ID && \
+                wpctl set-default $MIC_IN_ID
             ;;
         speakers)
-            # 57. Starship/Matisse HD Audio Controller Analog Stereo
-            wpctl set-default 57
+            wpctl set-default $SPEAKERS_ID && \
+                wpctl set-default $MIC_IN_ID
             ;;
         guitar)
-            # 113. alsa_input.usb-Allen___Heath_ZEDi10-00.Recording__Mic1__source
-            wpctl set-default 106 && \
-                wpctl set-default 113
+            wpctl set-default $MIXER_OUT_ID && \
+                wpctl set-default $GUITAR_IN_ID
             ;;
         # vr)
-        #     pactl set-default-sink ALVR\ Audio && \
-        #         pactl set-default-source ALVR\ Microphone
+        #     wpctl set-default VR_OUT_ID && \
+        #         wpctl set-default VR_IN_ID
         #     ;;
         *)
             ;;
